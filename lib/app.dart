51328e373a5d7682643477ba348007228be90a17
manques <anupbe15062011@gmail.com>
2023-07-31 06:55:32 +0530

    categories list

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meal_app/categories.dart';
-
+import 'package:flutter_meal_app/screens/categories/categories_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
-    seedColor: const Color.fromARGB(255, 0, 39, 35),
+    seedColor: const Color.fromARGB(255, 7, 236, 213),
     brightness: Brightness.dark,
   ),
   textTheme: GoogleFonts.latoTextTheme(),
